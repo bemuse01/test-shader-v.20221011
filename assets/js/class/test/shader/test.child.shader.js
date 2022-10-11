d292036bacfde565881232f077d357d095b67e54
bemuse01 <sequeland@gmail.com>
2022-10-11 15:39:54 +0900

    Update test.child.shader.js

--- assets/js/class/test/shader/test.child.shader.js
+++ assets/js/class/test/shader/test.child.shader.js
@@ -18,7 +18,10 @@ export default {
             vec2 coord = gl_FragCoord.xy;
             vec2 uv = coord / eResolution;
 
-            float y = cos(uv.x * PI) * 0.5 + 0.5;
+            float dist1 = 1.0 - distance(uv.x, 1.0);
+            float dist2 = 1.0 - distance(uv.x, 0.0);
+
+            float y = cos(uv.x * PI + time * 0.001) * dist1 * dist2 * 0.5 + 0.5;
             float a1 = smoothstep(y - 0.01, y, uv.y);
             float a2 = smoothstep(y + 0.01, y, uv.y);
 
