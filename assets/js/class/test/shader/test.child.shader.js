c004728377d14238639dc54f210c149283482dc9
bemuse01 <sequeland@gmail.com>
2022-10-12 11:44:25 +0900

    Update test.child.shader.js

--- assets/js/class/test/shader/test.child.shader.js
+++ assets/js/class/test/shader/test.child.shader.js
@@ -12,9 +12,9 @@ export default {
 
         const float PI = ${Math.PI};
         const float strength = 2.0;
-        const float minLinewidth = 0.005;
-        const float maxLinewidth = 0.0075;
-        const float opacity = 0.6;
+        const float minLinewidth = 0.01;
+        const float maxLinewidth = 0.02;
+        const float opacity = 0.25;
 
         float random(float t){
             return (cos(t) + cos(t * 1.3 + 1.3) + cos(t * 1.4 + 1.4)) / 3.0;   
@@ -29,28 +29,24 @@ export default {
 
             vec4 lines = vec4(vec3(1), 0.0);
 
-            // float n1 = snoise3D(vec3(uv * 0.1, time * 0.02)) * 0.5 + 0.5;
-            // float n2 = snoise3D(vec3(uv * 0.2, time * 0.02)) * 0.5 + 0.5;
-
-            // float scale = 0.25;
-            // float theta = 1.0 * PI * n1;
-            // float phi = acos(2.0 * n2 - 1.0);
-            // float velX = cos(theta) * sin(phi) * scale;
-            // float velY = sin(theta) * sin(phi) * scale;
-            // float velZ = cos(phi) * scale;
-
             for(float i = 0.0; i < 16.0; i += 1.0){
-                // float rand = velY;
-                float rand = random(i) * 0.5 + 0.5;
-                float str = (random(i) * 0.5 + 0.5) * 1.0 + 1.0;
+                float rand = random(i * 0.1) * 0.5 + 0.5;
+                float str = (random(i * 0.1) * 0.5 + 0.5) * 1.0 + 1.0;
                 // float offset = cos(i * PI * 0.1 + time) * 0.02;
-                float offset = random(i * time * 0.1) * 0.05;
+                float offset = random(i * 0.1) * 0.05;
                 float linewidth = mix(minLinewidth, maxLinewidth, rand);
 
                 float dist1 = 1.0 - distance(uv.x, 1.0) + offset;
                 float dist2 = 1.0 - distance(uv.x, 0.0) + offset;
+
+                // float theta = uv.x * PI + time * 0.3 * (i * 0.1);
+                // float phi = uv.x * PI * 2.0 + time * 0.3 * (i * 0.2);
+                float theta = uv.x * PI + time * 0.3;
+                float phi = uv.x * PI * 2.0 + time * 0.3;
+                float ry = sin(theta) * sin(phi);
     
-                float y = (cos(uv.x * PI + time * rand) * (dist1 * dist2 * str)) * 0.5 + 0.5 + offset; 
+                // float y = (cos(uv.x * PI + time * rand) * (dist1 * dist2 * str)) * 0.5 + 0.5 + offset; 
+                float y = (ry * (dist1 * dist2 * str)) * 0.5 + 0.5 + offset; 
                 float a1 = smoothstep(y - linewidth, y, uv.y);
                 float a2 = smoothstep(y + linewidth, y, uv.y);
                 
